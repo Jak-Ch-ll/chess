92ea00ad1fbdd2723d84d637fc3561b6eab583ef
J.c <Jak-Ch-ll@mailbox.org>
2023-07-07 17:40:15 +0200

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	type Figure = 'None' | 'Pawn' | 'Rook' | 'Knight' | 'Bishop' | 'Queen' | 'King';
+
+	const fields: Figure[][] = new Array(8).fill(new Array(8).fill('None'));
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
